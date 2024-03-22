30aa84a49a639f7bf49ad46c73fba261fe272aa6
komerajaya456 <komerajaya7259@gmail.com>
2024-03-22 15:29:29 +0530

    updated name

--- demo.java
+++ demo.java
@@ -2,8 +2,7 @@
 
 public class demo {
     public static void main(String[] args){
-    System.out.println("hello here anybody");
-    System.out.println("main chk  brached");
+    System.out.println("komera");
     
     }
 }
